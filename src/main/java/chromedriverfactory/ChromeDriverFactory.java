f6313fdf0bf8c047ad21929a8bb6a1572628dad3
Ivan Tyulkin <ivan.tyulkin@yahoo.com>
2023-06-15 16:21:41 +0300

    Corrections were added.

--- src/main/java/chromedriverfactory/ChromeDriverFactory.java
+++ src/main/java/chromedriverfactory/ChromeDriverFactory.java
@@ -2,8 +2,6 @@ package chromedriverfactory;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 
 public class ChromeDriverFactory {
 
